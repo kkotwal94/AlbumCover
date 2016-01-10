7b0f78dd082af43c1d2132aa904bbd840089ee20
kczer <kczer@udel.edu>
2016-01-10 04:14:32 +0000

    trying to get multiple discs working

--- main.js
+++ main.js
@@ -1,22 +1,43 @@
 jQuery(document).ready(function($){
 	var gallery = $('.openable'),
 		foldingPanel = $('.cd-folding-panel'),
-		mainContent = $('.cd-main');
+		mainContent = $('.cd-main'),
+		gallery2 = $('.op2');
+
 	/* open folding content */
 	gallery.on('click', 'a', function(event){
 		event.preventDefault(); //dont do whatever it does
 		openItemInfo($(this).attr('href')); //call openiteminfo
+		var myElement = document.querySelector(".fold-left");
+			myElement.style.backgroundImage = 'url("http://ime.ulximg.com/image/138/cover/1413338609_a82a511833773d95195be83d2a721e48.jpg/65d82aec9f9fa7f7e68a36d0b54210a0/1413338609_lil_b_the_basedgod_ultimate_bitch_mixtape_front_large_29.jpg")';
+						
+		//alert("opened");
 	});
+	
+	
+	gallery2.on('click', function(event){
+		event.preventDefault(); //dont do whatever it does
+		openItemInfo($(this).attr('href')); //call openiteminfo
+		//alert("opensd");
+		var myElement = document.querySelector(".fold-left");
+		myElement.style.backgroundImage = 'url("http://imd.ulximg.com/image/138/cover/1438798528_18d841f77a3ec52f84260a29f71e19ed.jpg/3b91f95bff809cabf27d034b8c8f9027/1438798528_0f5c1df0c0d753e69c99b3d9c2b3be19.jpg")';
+	});
+	
+	
+	
+
 
 	/* close folding content */
 	foldingPanel.on('click', '.cd-close', function(event){
 		event.preventDefault(); //stop doing whatever
 		toggleContent('', false); //untoggle whatever
 	});
+	
 	gallery.on('click', function(event){
 		/* detect click on .cd-gallery::before when the .cd-folding-panel is open */
 		if($(event.target).is('.cd-gallery') && $('.fold-is-open').length > 0 ) toggleContent('', false);
 	})
+	
 
 	function openItemInfo(url) {
 		var mq = viewportSize();
@@ -54,6 +75,7 @@ jQuery(document).ready(function($){
 		} else {
 			/* close the folding panel */
 			var mq = viewportSize();
+			$('body').removeClass('overflow-hidden');
 			foldingPanel.removeClass('is-open');
 			mainContent.removeClass('fold-is-open');
 			
